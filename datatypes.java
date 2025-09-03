9146326c455db3dd111c85ca57d393d0423cbb14
Jeevarasan S <jeevarasans77@gmail.com>
2025-09-03 11:54:02 +0530

    Create datatypes.java
    Datatypes in java

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32000;
+        int i = 100000;
+        long l = 123456789L;
+        float f = 3.14f;
+        double d = 3.14159265359;
+        char c = 'J';
+        boolean isJavaFun = true;
+        String message = "Hello, Java!";
+        int[] numbers = {1, 2, 3, 4, 5};  
+        }
+    }
