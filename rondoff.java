d344881d19a4a674905487b037ad0656bda6de90
Jeevarasan S <jeevarasans77@gmail.com>
2025-09-18 10:07:45 +0530

    Create rondoff.java
    it will print the roundoff value of the given number

---
+++ rondoff.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class rondoff{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float inputValue = scanner.nextFloat();
+        System.out.printf("%20.4f\n", inputValue);
+    }
+}
+
+
